55a8b253c8bdf062208bad906af125384ffd73c1
rodomanovt <timondeseptikon@gmail.com>
2025-03-18 14:51:26 +0300

    oop 2-4-1 code

--- oop-second-semester/2-4-1.cpp
+++ oop-second-semester/2-4-1.cpp
@@ -40,7 +40,7 @@ class Stack{
             }
         }
 
-        bool pop(int* val){ // значение записать в val
+        bool pop(int* val){ // значение записывается в val
             if (cntItems > 0){
                 *val = data[cntItems-1];
                 cntItems -= 1;
@@ -54,23 +54,17 @@ class Stack{
 
 
 int main(){
-    // Stack s = Stack("test", 3);
-    // if(s.push(420)) cout << 1 << endl;
-    // int val = 0;
-    // if(s.pop(&val)) cout << 2 << endl;
-    // cout << val;
-
     cout.width(15);
     cout.setf(ios::left);
 
     string name1; int size1;
-    cout << "s1 name and size: ";
+    //cout << "s1 name and size: ";
     cin >> name1 >> size1;
     Stack s1 = Stack(name1, size1);
-    cout << s1.getName() << size1 << endl;
+    cout << setw(15) << s1.getName() << size1 << endl;
 
     int input;
-    cout << "s1 el: ";
+    //cout << "s1 el: ";
     while (cin >> input) {
         if (input == 0) {
             break;
@@ -79,12 +73,12 @@ int main(){
     }
 
     string name2; int size2;
-    cout << "s2 name and size: ";
+    //cout << "s2 name and size: ";
     cin >> name2 >> size2;
     Stack s2 = Stack(name2, size2);
-    cout << s2.getName() << size2 << endl;
+    cout << setw(15) << s2.getName() << size2 << endl;
 
-    cout << "s2 el: ";
+    //cout << "s2 el: ";
     while (cin >> input) {
         if (input == 0) {
             break;
@@ -92,13 +86,13 @@ int main(){
         s2.push(input);
     }
     
-    cout << s1.getName() << s2.getName() << endl;
+    cout << setw(15) << s1.getName() << setw(15) << s2.getName() << endl;
+    cout.setf(ios::right);
     while (s1.getCntItems() > 0 && s2.getCntItems() > 0){
-        cout << 1;
         int val1, val2;
         s1.pop(&val1);
         s2.pop(&val2);
-        cout << val1 << val2 <<endl;
+        cout << setw(15) << val1 << "    ["<< setw(15) << val2 << "]" <<endl;
    }
 
 
